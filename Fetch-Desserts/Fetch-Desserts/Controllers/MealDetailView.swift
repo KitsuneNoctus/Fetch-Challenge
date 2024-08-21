5c87d5332e28f3648b711f5afa872390a7ec31bd
KitsuneNoctus <theceintiant@gmail.com>
2024-08-21 16:07:15 -0700

    Rough draft of food detail view 2

--- Fetch-Desserts/Fetch-Desserts/Controllers/MealDetailView.swift
+++ Fetch-Desserts/Fetch-Desserts/Controllers/MealDetailView.swift
@@ -20,14 +20,77 @@ struct MealDetailView: View {
                     ProgressView()
                 }
                 HStack {
-                    Text("Tags")
+                    Text("Tags: \(exampleMealDetailModel.strTags)")
                     Spacer()
-                    Text("Area")
+                    Text(exampleMealDetailModel.strArea)
                 }
+                .padding(5)
                 Text("Video")
+                    .padding(5)
                 Text("Ingredients")
+                    .font(.title3)
+                    .padding(5)
+                
+                HStack {
+                    Text(("\u{2022}"))
+                    Text(exampleMealDetailModel.strMeasure1!)
+                    Text(exampleMealDetailModel.strIngredient1!)
+                }
+                .padding(.horizontal, 5)
+                HStack {
+                    Text(("\u{2022}"))
+                    Text(exampleMealDetailModel.strMeasure2!)
+                    Text(exampleMealDetailModel.strIngredient2!)
+                }
+                .padding(.horizontal, 5)
+                HStack {
+                    Text(("\u{2022}"))
+                    Text(exampleMealDetailModel.strMeasure3!)
+                    Text(exampleMealDetailModel.strIngredient3!)
+                }
+                .padding(.horizontal, 5)
+                HStack {
+                    Text(("\u{2022}"))
+                    Text(exampleMealDetailModel.strMeasure4!)
+                    Text(exampleMealDetailModel.strIngredient4!)
+                }
+                .padding(.horizontal, 5)
+                HStack {
+                    Text(("\u{2022}"))
+                    Text(exampleMealDetailModel.strMeasure5!)
+                    Text(exampleMealDetailModel.strIngredient5!)
+                }
+                .padding(.horizontal, 5)
+                HStack {
+                    Text(("\u{2022}"))
+                    Text(exampleMealDetailModel.strMeasure6!)
+                    Text(exampleMealDetailModel.strIngredient6!)
+                }
+                .padding(.horizontal, 5)
+                HStack {
+                    Text(("\u{2022}"))
+                    Text(exampleMealDetailModel.strMeasure7!)
+                    Text(exampleMealDetailModel.strIngredient7!)
+                }
+                .padding(.horizontal, 5)
+                if exampleMealDetailModel.strIngredient8 != "" {
+                    HStack {
+                        Text(("\u{2022}"))
+                        Text("Amount")
+                        Text("Ingredient")
+                    }
+                    .padding(.horizontal, 5)
+                }
+                
+                
+                
                 Text("Instructions")
+                    .font(.title3)
+                    .padding(5)
+                Text(exampleMealDetailModel.strInstructions)
+                    .padding()
                 Text("Source")
+                    .padding()
             }
         }
         .navigationTitle("Banana Pancakes")
