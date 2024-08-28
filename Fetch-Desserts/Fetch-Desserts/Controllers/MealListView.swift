1d0bf0e78eb0ee81581a3212789d9c99f03f86ec
KitsuneNoctus <theceintiant@gmail.com>
2024-08-28 12:21:40 -0700

    Cleaning up project, Getting ready for submission

--- Fetch-Desserts/Fetch-Desserts/Controllers/MealListView.swift
+++ Fetch-Desserts/Fetch-Desserts/Controllers/MealListView.swift
@@ -11,17 +11,10 @@ struct MealListView: View {
     
     @StateObject private var mealListViewModel: MealListViewModel = MealListViewModel()
     
-//    private var mealSelector = MealTypeSelector(selectedMealType: <#Binding<String>#>)
-    
-    @State private var selectedMealType = "Dessert"
-    
     var body: some View {
         NavigationStack {
             ScrollView {
                 VStack(alignment: .leading) {
-                    MealTypeSelector(selectedMealType: $selectedMealType) {
-                        mealListViewModel.fetchMeals(for: selectedMealType)
-                    }
                     ForEach(mealListViewModel.meals, id: \.idMeal) { meal in
                         NavigationLink(destination: MealDetailView(mealID: meal.idMeal)) {
                             MealListCell(mealModel: meal)
